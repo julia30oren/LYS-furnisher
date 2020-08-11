ea45452da8c02bff89d6fb8fcff4a27979ec4a6b
julia30oren <juliaoren30@gmail.com>
2020-08-11 14:07:17 +0300

    coating done

--- react-app/src/components/paiges/Contacts.jsx
+++ react-app/src/components/paiges/Contacts.jsx
@@ -34,15 +34,17 @@ export const ContactUs = (props) => {
                         <p>{translate('contacts-7-text')}</p>
                         <p>{translate('contacts-8-text')}</p>
                     </div>
-                    <div>
-                        <iframe title="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2577.506675436036!2d24.009255315877372!3d49.75772544491529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDQ1JzI3LjgiTiAyNMKwMDAnNDEuMiJF!5e0!3m2!1sen!2sil!4v1597038061534!5m2!1sen!2sil" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"
-                            style={{
-                                border: "0",
-                                height: '200px',
-                                width: '100%',
-                                paddingLeft: '10%'
-                            }}></iframe>
-                    </div>
+
+                    <iframe title="googleMap" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2577.506675436036!2d24.009255315877372!3d49.75772544491529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDQ1JzI3LjgiTiAyNMKwMDAnNDEuMiJF!5e0!3m2!1sen!2sil!4v1597038061534!5m2!1sen!2sil"
+                        style={{
+                            border: "0.5px solid black",
+                            height: '200px',
+                            width: '89%',
+                            margin: '3%'
+                        }}>
+                    </iframe>
+
                 </div>
             </div>
 
