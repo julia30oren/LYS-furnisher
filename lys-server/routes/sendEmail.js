5199c33f111f3134589a7a89c7a91864db24e958
julia30oren <juliaoren30@gmail.com>
2020-08-27 15:45:21 +0300

    ready for testing

--- lys-server/routes/sendEmail.js
+++ lys-server/routes/sendEmail.js
@@ -1,54 +1,60 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-require("dotenv").config()
-const userValidation = require("../validations/validateUser")
+require("dotenv").config();
+const userValidation = require("../validations/validateUser");
 
-router.use(userValidation)
+router.use(userValidation);
 
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
 router.post("/contact", async(req, res, next) => {
-    const main = async() => {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.SMTPHOSTEMAILUSER,
-                pass: process.env.SMTPHOSTEMAILPASSWORD,
-            }
-            // ,
-            // tls: {
-            //     rejectUnauthorized: false
-            // }
-        });
+    try {
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: process.env.SMTPHOSTEMAILUSER, // sender address
-            to: process.env.DESIGNATEDSUPPORTEMAIL, // list of receivers
-            // text: "Lorem", // plain text body
-            html: `
-            <h2>Contact Request</h2>
-            <h3>Contact Details</h3>
-            <ul>  
-              <li>Name: ${req.body.name}</li>
-              <li>Contact Method: ${req.body.email || req.body.phone}</li>
-            </ul>
-            <h3>Message:</h3>
-            <p>${req.body.text}</p>
-          `
-        });
-
-        console.log("Message sent: %s", info.messageId);
-        if (info.messageId) return res.status(200).json({ message: "Email Sent" })
+        const email = await main(req.body)
+        console.log(email, "email")
+        if (email) return res.json({ message: "Email Sent" })
+    } catch (error) {
+        console.log(error.message)
     }
-    main().catch(console.error);
 })
 
 
+module.exports = router;
 
 
-module.exports = router
+const main = async(obj) => {
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTPHOSTEMAILUSER,
+            pass: process.env.SMTPHOSTEMAILPASSWORD,
+        }
+        // ,
+        // tls: {
+        //     rejectUnauthorized: false
+        // }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: process.env.SMTPHOSTEMAILUSER, // sender address
+        to: 'lys.furniture@gmail.com', // list of receivers
+        subject: `👻Сообщение с сайта`, // Subject line
+        html: `
+        <div style="border: green 1px solid; padding: 5%">
+            <h2>Contact Request</h2>
+            <ul>  
+                <li>Имя: ${obj.name}</li>
+                <li>Контактный метод: ${obj.email || obj.phone}</li>
+            </ul>
+            <h3>Сообщение:</h3>
+            <p>${obj.text}</p>
+        </div>
+          `
+    });
+    return info.messageId;
+}
